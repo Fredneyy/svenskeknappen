ee89b25f6c4058b76a65ca38a9957e43a5b4236f
Fredneyy <69763370+Fredneyy@users.noreply.github.com>
2025-03-12 19:10:17 +0100

    Added sound effects

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 "use client";
+import { useState } from "react";
+
 export default function Home() {
+  let turnOn: HTMLAudioElement;
+  let turnOff: HTMLAudioElement;
+
+  const [isOn, setIsOn] = useState(true);
+
+  if (typeof window !== "undefined") {
+    turnOn = new Audio("/sounds/windowsXPOn.mp3");
+    turnOff = new Audio("/sounds/windowsXPOff.mp3");
+  }
+
   function handleClick(e: React.MouseEvent<HTMLButtonElement>, type: string) {
     e.preventDefault();
 
@@ -10,16 +22,30 @@ export default function Home() {
     }
   }
 
+  function clickActionHandler(e: React.MouseEvent<HTMLButtonElement>) {
+    if (isOn) {
+      turnOff.play();
+    } else {
+      turnOn.play();
+    }
+    setIsOn((prev) => !prev);
+  }
+
   return (
     <>
-      <main className="min-h-screen">
-        <div className="w-[190px] mx-auto mb-[25px] mt-40">
+      <main
+        className={`min-h-screen pb-[25px] pt-40 transition-colors duration-1000 ${
+          isOn ? "bg-white" : "bg-black"
+        }`}
+      >
+        <div className="w-[190px] mx-auto">
           <div className="rounded-full w-[188px] h-[174px] mt-[7px] ml-[6px] absolute bg-[#FF0000]"></div>
           <button
             className="w-[200px] h-[190px] absolute bg-[url(/transparent_button_sprite.png)] bg-[position:-5px_-5px] bg-no-repeat bg- border-0 block [-webkit-tap-highlight-color: transparent;]"
             title="Svenskeknappen"
             onMouseDown={(e) => handleClick(e, "down")}
             onMouseUp={(e) => handleClick(e, "up")}
+            onClick={clickActionHandler}
           ></button>
           <div className="w-[200px] h-[190px] bg-[url(/transparent_button_shadow.png)] bg-no-repeat"></div>
         </div>
